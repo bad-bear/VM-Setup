b03368903e6fe14da90da8e8412c980e022a3fa5
Zion James <zionj00@outlook.com>
2021-05-03 21:14:06 -0500

    Updated for the media branch

--- setup.sh
+++ setup.sh
@@ -1,17 +1,14 @@
 #! /bin/bash
 
-sudo apt install git snapd ansible ruby-full python3 python3-venv python3-pip
+sudo apt install git snapd ansible python3 python3-venv python3-pip open-vm-tools
 sudo snap install code --classic
-sudo snap install node --classic
-sudo snap install umbrello --classic
 sudo snap install gitkraken --classic
 sudo snap install chromium --classic
 sudo snap install krita --classic
-sudo snap install micro --classic
-sudo snap install kotlin --classic
-sudo snap install postman --classic
+sudo snap install audacity --classic
+sudo snap install blender --classic
+sudo snap install focuswriter --classic
 
-sudo apt-get install golang-go
 
 wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
@@ -19,20 +16,6 @@ sudo apt-get update
 sudo apt-get install -y mongodb-org
 sudo systemctl start mongod
 sudo systemctl enable mongod
-
-wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-sudo dpkg -i packages-microsoft-prod.deb
-sudo apt-get update;
-sudo apt-get install -y apt-transport-https
-sudo apt-get update
-sudo apt-get install -y dotnet-sdk-5.0
-
-sudo apt-get update;
-sudo apt-get install -y apt-transport-https
-sudo apt-get update
-sudo apt-get install -y aspnetcore-runtime-5.0
-
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 sudo apt-get update
 sudo apt-get install apt-transport-https
@@ -48,14 +31,4 @@ wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-
 sudo apt-get update
 sudo apt-get install postgresql
 
-
-sudo apt-get remove docker docker-engine docker.io containerd runc
-sudo apt-get update
-sudo apt-get install apt-transport-https ca-certificates curl gnupg lsb-release
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
-sudo groupadd docker
-sudo usermod -aG docker $USER
 shutdown -r now
